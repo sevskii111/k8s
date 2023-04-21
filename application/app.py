fc259ca9f1a42dc29522786d13d7c7aa74203f8e
sevskii <sevskii111@gmail.com>
2023-04-22 01:07:19 +0300

    add initial helm chart

--- application/app.py
+++ application/app.py
@@ -5,8 +5,11 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
+redis_host = os.environ['DATABASE_URL']
+redis_password = os.environ['DATABASE_PASSWORD']
+redis_port=os.environ['DATABASE_PORT']
+cache = redis.Redis(host=redis_host, password=redis_password,  port=redis_port)
 
 def get_hit_count():
     retries = 5
